5bc7c49cfa163c8594f135b626a6a352436eaf38
Mohammed Raheez <mohammedraheez@Raheez-MAC.local>
2025-03-09 15:16:34 +0530

    homepage

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mypyramidexam-frontend/',
+  base: '/mypyramidexam/',
   plugins: [react(),tailwindcss()],
 })
